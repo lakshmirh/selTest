d8b6fb588914f29a965b3fbf04ffda43be2b1295
lakshmirh <thames221@outlook.com>
2017-04-18 14:25:31 +0100

    Contact us testing
    Contact us feature of a webpage testing

---
+++ UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using System.Collections.ObjectModel;
+
+namespace Wikipedia
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new FirefoxDriver();
+        public void GoToContactUsPage()
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("http://www.acumenci.com");
+                driver.Manage().Window.Maximize();
+                Thread.Sleep(2000);
+                driver.FindElement(By.XPath(".//*[@id='masthead']/button")).Click();
+                Thread.Sleep(3000);
+                driver.FindElement(By.XPath(".//*[@id='menu-item-74']/a")).Click();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                driver.Quit();
+            }
+
+        }
+
+        public void CloseDriver()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void AccurateAddress()
+        {
+            try
+            {
+                GoToContactUsPage();
+                String Address = driver.FindElement(By.XPath(".//*[@id='contact_anchor']/div[3]/div/p[1]/a")).Text;
+                Console.WriteLine(Address);
+                if (Address.Contains("TW9 1HY"))
+                    Console.WriteLine("Address is validated");
+                else
+                    Assert.Fail();
+                CloseDriver();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+
+            }
+
+
+        }
+
+
+        [TestMethod]
+        public void SubmissionValidation()
+        {
+            try
+            {
+                GoToContactUsPage();
+                Thread.Sleep(2000);
+                driver.FindElement(By.XPath(".//*[@id='content']/div[2]/a[3]/div/h3")).Click();
+                Thread.Sleep(2000);
+
+                ReadOnlyCollection<IWebElement> JobOpportunity = driver.FindElements(By.ClassName("section-heading"));
+
+                String value;
+                for (int k = 0; k < JobOpportunity.Count; k++)
+                {
+                    value = JobOpportunity[k].Text;
+                    if (value.Contains("tester"))
+                    {
+                        Console.WriteLine("tester found");
+                        break;
+                    }
+                    else
+                        Console.WriteLine("Not found");
+
+                }
+                CloseDriver();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+    }
+}
